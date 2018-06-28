a58d8ba5f426067b05eaf107009c578bf20ebbc7
Kurt <k@kcmr.io>
2018-06-27 18:58:21 -0700

    Removes comments at the bottom.

--- 210_js_programming/rotThirteen.js
+++ 210_js_programming/rotThirteen.js
@@ -15,9 +15,3 @@ function rotThirteen(string) {
   }
   return newString;
 }
-
-
-console.log(rot13('Teachers open the door, but you must enter by yourself.'));
-
-// logs:
-Grnpuref bcra gur qbbe, ohg lbh zhfg ragre ol lbhefrys.
